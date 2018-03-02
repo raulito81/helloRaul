65609f331b11127d34f7c215aa5d7cd3c343998a
Raul Maroto <raul.marotocarrera@telefonica.com>
2018-03-02 08:06:58 +0100

    aaaaaa

--- helloRaulito/main.py
+++ helloRaulito/main.py
@@ -1,3 +1,3 @@
 def main():
-    print("Hello, world!!")
+    print("Hello, Raul!!")
     return True
---
+++ hello_Raulito.py
@@ -0,0 +1,4 @@
+import helloRaulito.main
+
+if __name__ == '__main__':
+    helloRaulito.main.main()
