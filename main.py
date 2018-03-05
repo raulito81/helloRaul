a273907c23256ba8d57631927f9bea0c748c0d02
Raul Maroto <raul.marotocarrera@telefonica.com>
2018-03-05 16:28:47 +0100

    change

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from datetime import datetime
 def main():
-    print("Hello, Raul!!")
+    print("""
+    Hello, Raul!!
+    Esto es una prueba,
+    Solo vamos a pintar en pantalla este mensaje
+    que se ha generado en esta fecha: {}
+    """.format(datetime.now()))
     return True if True else False
